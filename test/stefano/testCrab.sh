ee46dbcf54e698891080c10309760ef03e3f6d60
Stefano Belforte <stefano.belforte@cern.ch>
2023-10-11 23:40:25 +0200

    not use IB

--- test/stefano/testCrab.sh
+++ test/stefano/testCrab.sh
@@ -6,7 +6,7 @@ id
 
 echo "++ setup CMSSW"
 
-source /cvmfs/cms-ib.cern.ch/latest/cmsset_default.sh
+source /cvmfs/cms.cern.ch/latest/cmsset_default.sh
 scramv1 project ${CMSSW_rel}
 cd ${CMSSW_rel}
 eval `scramv1 runtime -sh`

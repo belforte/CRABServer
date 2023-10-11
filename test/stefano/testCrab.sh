79dc6ad29c0a0aa5beb8294dce8b33ebe3845853
Stefano Belforte <stefano.belforte@cern.ch>
2023-10-11 23:50:03 +0200

    diag

--- test/stefano/testCrab.sh
+++ test/stefano/testCrab.sh
@@ -6,17 +6,22 @@ id
 
 echo "++ setup CMSSW"
 
-source /cvmfs/cms.cern.ch/latest/cmsset_default.sh
+source /cvmfs/cms.cern.ch/cmsset_default.sh
 scramv1 project ${CMSSW_rel}
 cd ${CMSSW_rel}
 eval `scramv1 runtime -sh`
+echo $CMSSW_VERSION
+echo $SCRAM_ARCH
 
 cd ..
 
 echo "++ test crab"
+echo "++ crab --version"
 crab --version
 
+echo "++ crab checkusername"
 crab checkusername
 
+echo "++ crab checkwrite --site T2_CH_CERN"
 crab checkwrite --site T2_CH_CERN
 

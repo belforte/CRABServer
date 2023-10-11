42d2c4ca5b75aacee11f884eeeda24d1d06854d7
Stefano Belforte <stefano.belforte@cern.ch>
2023-10-11 22:44:55 +0200

    add diag

--- test/stefano/testRucio.sh
+++ test/stefano/testRucio.sh
@@ -1,7 +1,16 @@
+#!/bin/bash
 voms-proxy-info
+echo "++ Who am I ?"
 whoami
 id
+echo "++ source /cvmfs/cms.cern.ch/rucio/setup-py3.sh"
 source /cvmfs/cms.cern.ch/rucio/setup-py3.sh
+
+echo "++ force RUCIO_ACCOUNT to cmsbot"
 export RUCIO_ACCOUNT=`whoami`
 export RUCIO_ACCOUNT=cmsbot
-rucio whoami
+echo "++ RUCIO_ACCOUNT= $RUCION_ACCOUNT"
+
+echo "++ test rucio whoami"
+rucio whoami && echo SUCCESS
+echo ""

77c5ba5568eed90ba38a158f87a03f45600eccd5
Stefano Belforte <stefano.belforte@cern.ch>
2023-10-11 23:29:35 +0200

    add testCrab.sh

---
+++ test/stefano/testCrab.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+voms-proxy-info
+echo "++ Who am I ?"
+whoami
+id
+
+echo "++ setup CMSSW"
+
+source /cvmfs/cms-ib.cern.ch/latest/cmsset_default.sh
+scramv1 project ${CMSSW_rel}
+cd ${CMSSW_rel}
+eval `scramv1 runtime -sh`
+
+cd ..
+
+echo "++ test crab"
+crab --version
+
+crab checkusername
+
+crab checkwrite --site T2_CH_CERN
+

bb610997297b48dc0533339f442e50827c9a20a1
Dario Mapelli <mapelli.dario@gmail.com>
2024-05-27 18:50:01 +0200

    monit - generatemonit on pypi image

--- cicd/build/monit-taskworker/monit_build.sh
+++ cicd/build/monit-taskworker/monit_build.sh
@@ -2,12 +2,12 @@
 
 ## bash ./cicd/build/monit-taskworker/monit_build.sh v3.240111
 
-TAG=v3.240325
+TAG=pypi-test12-1716447194
 
 docker build \
   --build-arg "TAG=$TAG" \
   --network=host \
-  -t registry.cern.ch/cmscrab/crabtaskworker:$TAG.monittw \
+  -t registry.cern.ch/cmscrab/crabtaskworker:$TAG-monittw \
   ./ \
   -f ./cicd/build/monit-taskworker/Dockerfile
 

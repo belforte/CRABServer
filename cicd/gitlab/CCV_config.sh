7002b143c96e743a3aaf118170ea4d6bed2ac6e8
Stefano Belforte <stefano.belforte@cern.ch>
2026-03-11 18:09:16 +0100

    always use CMSSW_15 on el8 to run CCV

--- cicd/gitlab/CCV_config.sh
+++ cicd/gitlab/CCV_config.sh
@@ -21,31 +21,43 @@ if [ ! -d "$WORK_DIR" ]; then
 fi
 pushd "${WORK_DIR}"
 
+# use CMSSW_15 and python3 to run CCV tests, they are independent of which
+# releae was used for submitting
+CMSSW_release=CMSSW_15_0_18
+SCRAM_ARCH=el8_amd64_gcc12
+scramprefix=el8
 
-# Get configuration from CMSSW_release
-CONFIG_LINE="$(grep "CMSSW_release=${CMSSW_release};" "${ROOT_DIR}"/test/testingConfigs)"
-SCRAM_ARCH="$(echo "${CONFIG_LINE}" | tr ';' '\n' | grep SCRAM_ARCH | sed 's|SCRAM_ARCH=||')"
-inputDataset="$(echo "${CONFIG_LINE}" | tr ';' '\n' | grep inputDataset | sed 's|inputDataset=||')"
-# see https://github.com/dmwm/WMCore/issues/11051 for info about SCRAM_ARCH formatting
-singularity="$(echo "${SCRAM_ARCH}" | cut -d"_" -f 1 | tail -c 2)"
-export SCRAM_ARCH inputDataset singularity
 
-chmod +x ${ROOT_DIR}/cicd/gitlab/clientConfigurationValidation.sh
-if [ "X${singularity}" == X6 ] || [ "X${singularity}" == X7 ] || [ "X${singularity}" == X8 ]; then
-    echo "Starting singularity ${singularity} container."
-    if [ "X${singularity}" == X6 ]; then scramprefix=cc${singularity}; fi
-    if [ "X${singularity}" == X7 ]; then scramprefix=el${singularity}; fi
-    if [ "X${singularity}" == X8 ]; then scramprefix=el${singularity}; fi
-    ERR=false;
-    /cvmfs/cms.cern.ch/common/cmssw-${scramprefix} -- "${ROOT_DIR}/cicd/gitlab/clientConfigurationValidation.sh"
-    CCV_EC=$?
-    echo "clientConfigurationValidation.sh script ended wih exit code $CCV_EC"
-    ERR=$([[ $CCV_EC -eq 0 || $CCV_EC -eq 2 ]] && echo "false" || echo "true") #ERR is true if return is other than 0 or 2
-    Check=
-else
-    echo "!!! I am not prepared to run for slc${singularity}."
-    exit 1
-fi
+ERR=false;
+/cvmfs/cms.cern.ch/common/cmssw-${scramprefix} -- "${ROOT_DIR}/cicd/gitlab/clientConfigurationValidation.sh"
+CCV_EC=$?
+echo "clientConfigurationValidation.sh script ended wih exit code $CCV_EC"
+ERR=$([[ $CCV_EC -eq 0 || $CCV_EC -eq 2 ]] && echo "false" || echo "true") #ERR is true if return is other than 0 or 2
+
+
+## Get configuration from CMSSW_release
+#CONFIG_LINE="$(grep "CMSSW_release=${CMSSW_release};" "${ROOT_DIR}"/test/testingConfigs)"
+#SCRAM_ARCH="$(echo "${CONFIG_LINE}" | tr ';' '\n' | grep SCRAM_ARCH | sed 's|SCRAM_ARCH=||')"
+#inputDataset="$(echo "${CONFIG_LINE}" | tr ';' '\n' | grep inputDataset | sed 's|inputDataset=||')"
+## see https://github.com/dmwm/WMCore/issues/11051 for info about SCRAM_ARCH formatting
+#singularity="$(echo "${SCRAM_ARCH}" | cut -d"_" -f 1 | tail -c 2)"
+#export SCRAM_ARCH inputDataset singularity
+
+#chmod +x ${ROOT_DIR}/cicd/gitlab/clientConfigurationValidation.sh
+#if [ "X${singularity}" == X6 ] || [ "X${singularity}" == X7 ] || [ "X${singularity}" == X8 ]; then
+#    echo "Starting singularity ${singularity} container."
+#    if [ "X${singularity}" == X6 ]; then scramprefix=cc${singularity}; fi
+#    if [ "X${singularity}" == X7 ]; then scramprefix=el${singularity}; fi
+#    if [ "X${singularity}" == X8 ]; then scramprefix=el${singularity}; fi
+#    ERR=false;
+#    /cvmfs/cms.cern.ch/common/cmssw-${scramprefix} -- "${ROOT_DIR}/cicd/gitlab/clientConfigurationValidation.sh"
+#    CCV_EC=$?
+#    echo "clientConfigurationValidation.sh script ended wih exit code $CCV_EC"
+#    ERR=$([[ $CCV_EC -eq 0 || $CCV_EC -eq 2 ]] && echo "false" || echo "true") #ERR is true if return is other than 0 or 2
+#else
+#    echo "!!! I am not prepared to run for slc${singularity}."
+#    exit 1
+#fi
 
 if [ "$ERR" == true ]; then
     echo "clientConfigurationValidation.sh script failed to run properly."
@@ -58,15 +70,15 @@ fi
 # when all tests have run (no retry) if at least one test failed, CCV failed
 # otherwise CCV is successful
 
-if [ -s "retry_tests_${CI_PIPELINE_ID}_${CMSSW_release}" ]; then
+if [ -s "retry_tests" ]; then
     # file size is > 0
     TEST_RESULT='FULL-STATUS-UNKNOWN'  # means: wait and then run tests again
 else
   # no more retries are needed
-  if [ -s "failed_tests_${CI_PIPELINE_ID}_${CMSSW_release}" ]; then
+  if [ -s "failed_tests" ]; then
     # file not empty, some tests failed
     TEST_RESULT='FAILED'
-  elif [ -s "successful_tests_${CI_PIPELINE_ID}_${CMSSW_release}" ]; then
+  elif [ -s "successful_tests" ]; then
     # sanity check... this file better be not empty now !
     TEST_RESULT='SUCCEEDED'
   else
